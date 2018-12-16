9646dc82861c462581419fdf636981f32e2101d4
Dave Garwacke <admin+git@ifyoumakeit.com>
2018-12-16 12:45:46 -0500

    Highlight clue

--- src/App.js
+++ src/App.js
@@ -37,29 +37,52 @@ function isBlack(val) {
   return val === ".";
 }
 
-function getCluesLookup(grid, gridnums, rows) {
-  return grid.map((letter, index) => {
-    if (letter === ".") {
-      return 0;
+function getCluesLookup(grid, gridnums, cols) {
+  return grid.reduce(
+    (acc, letter, index) => {
+      if (letter === ".") {
+        return {
+          ...acc,
+          across: acc.across.concat(null)
+        };
+      }
+
+      // Look through row for first sign of .
+      const acrossRowCurrent = Math.floor(index / cols);
+      const acrossRowOffset = cols * acrossRowCurrent;
+      const acrossSlice = grid.slice(acrossRowOffset, index);
+      const acrossDotOffset = acrossSlice
+        .reverse()
+        .findIndex(val => val === ".");
+      const acrossDotIndex =
+        acrossDotOffset < 0
+          ? acrossRowOffset
+          : acrossRowOffset + acrossSlice.length - acrossDotOffset;
+      const acrossClueNumber = gridnums[acrossDotIndex];
+      const acrossCounter =
+        acc.acrossNumber === acrossClueNumber
+          ? acc.acrossCounter
+          : acc.acrossCounter + 1;
+
+      return {
+        ...acc,
+        acrossCounter,
+        acrossNumber: acrossClueNumber,
+        across: acc.across.concat(acrossCounter)
+      };
+    },
+    {
+      acrossNumber: 0,
+      acrossCounter: 0,
+      across: [],
+      downNumber: 0,
+      downCounter: 0,
+      down: []
     }
-
-    const col = Math.floor(index / rows);
-    const offset = col * rows;
-    const slice = grid.slice(offset, index);
-
-    // Look through row for first sign of .
-    const numberAssocIndex = slice.reverse().findIndex(val => val === ".");
-
-    const numberAssocIndexNormalized =
-      numberAssocIndex < 0 ? offset : offset + slice.length - numberAssocIndex;
-
-    const numberAssoc = gridnums[numberAssocIndexNormalized];
-    return numberAssoc;
-  });
+  );
 }
 
-function App(props) {
-  const { size, grid, gridnums } = props;
+function App({ size, grid, gridnums, ...props }) {
   const cells = size.cols * size.rows;
 
   // Create updaters for all pieces of state
@@ -69,7 +92,7 @@ function App(props) {
     Array.from({ length: cells }, () => "")
   );
   const [cluesLookup, setCluesLookup] = useState(
-    getCluesLookup(grid, gridnums, size.rows)
+    getCluesLookup(grid, gridnums, size.cols)
   );
 
   // Set references to all inputs, skip black cells.
@@ -86,7 +109,7 @@ function App(props) {
       }
 
       // Update clues if they change
-      setCluesLookup(getCluesLookup(grid, gridnums, size.rows));
+      setCluesLookup(getCluesLookup(grid, gridnums, size.cols));
 
       return () => window.removeEventListener("keydown", handleKeyDown);
     },
@@ -127,6 +150,7 @@ function App(props) {
         }
         return event.shiftKey ? go(DIRECTIONS.up) : go(DIRECTIONS.down);
       }
+
       default: {
         return;
       }
@@ -138,7 +162,7 @@ function App(props) {
       <section className="crossword" style={{ "--rows": size.rows }}>
         {grid.map((letter, index) => {
           const number = gridnums[index];
-          const clue = cluesLookup[index];
+          const clue = cluesLookup.across[index];
 
           return (
             <div
@@ -146,12 +170,12 @@ function App(props) {
               className="cell"
               style={{
                 "--bgcolor": isBlack(letter)
-                  ? "black"
+                  ? "#000000"
                   : index === indexCurrent
                   ? "#efefef"
-                  : clue === cluesLookup[indexCurrent]
-                  ? "#eaeaea"
-                  : "white"
+                  : clue === cluesLookup.across[indexCurrent]
+                  ? "#dddddd"
+                  : "#ffffff"
               }}
             >
               {number > 0 && <span className="num">{number}</span>}
@@ -161,27 +185,28 @@ function App(props) {
                   name={`cell-${index}`}
                   value={letters[index]}
                   ref={refs[index]}
-                  onFocus={() => {
-                    setIndexCurrent(index);
+                  onFocus={() => setIndexCurrent(index)}
+                  onChange={() => {
+                    /* Handled by keydown */
                   }}
-                  onChange={event => {
-                    const { value } = event.currentTarget;
-                    const valueSingle = value[value.length - 1];
+                  onKeyDown={event => {
+                    const key = event.key;
+                    const value = key === "Backspace" ? "" : key;
 
-                    if (!/[A-z]/.test(valueSingle)) {
-                      return; // Skip non-letters.
+                    if (value && !/^[a-zA-Z]$/.test(value)) {
+                      return; // Skip non-letters or multi-letter keys.
                     }
 
                     setLetters([
                       ...letters.slice(0, index),
-                      valueSingle.toUpperCase(),
+                      value.toUpperCase(),
                       ...letters.slice(index + 1)
                     ]);
 
                     if (isAcross) {
-                      return go(DIRECTIONS.right);
+                      return value ? go(DIRECTIONS.right) : go(DIRECTIONS.left);
                     }
-                    return go(DIRECTIONS.down);
+                    return value ? go(DIRECTIONS.down) : go(DIRECTIONS.up);
                   }}
                 />
               )}
@@ -189,6 +214,29 @@ function App(props) {
           );
         })}
       </section>
+      <h1>Clues</h1>
+
+      {["across", "down"].map(key => (
+        <section key={key}>
+          <h1>{key}</h1>
+          <ul>
+            {props.clues[key].map((clue, index) => (
+              <li
+                key={clue}
+                className="clue"
+                style={{
+                  "--bgcolor":
+                    cluesLookup.across[indexCurrent] - 1 === index
+                      ? "#efefef"
+                      : "#ffffff"
+                }}
+              >
+                {clue}
+              </li>
+            ))}
+          </ul>
+        </section>
+      ))}
     </main>
   );
 }
