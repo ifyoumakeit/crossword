0881361bacf7f4a47b7b4758c5bd161fe96cdf90
Dave Garwacke <dave.garwacke@warbyparker.com>
2019-02-05 12:50:37 -0500

    Use clues to get index
    This is getting a little confusing but we can use the clue index
    to find the index its named in the matrix.

--- src/Crossword/index.js
+++ src/Crossword/index.js
@@ -32,107 +32,102 @@ function App({
     !utils.isBlack(letter) ? useRef(null) : null
   );
 
-  useEffect(
-    () => {
-      // Focus on next input if index changes.
-      if (refs[state.index] && refs[state.index].current) {
-        refs[state.index].current.focus();
-      }
-    },
-    [state.index]
-  );
+  useEffect(() => {
+    // Focus on next input if index changes.
+    if (refs[state.index] && refs[state.index].current) {
+      refs[state.index].current.focus();
+    }
+  }, [state.index]);
 
-  useEffect(
-    () => {
-      window.XW = {
-        next: () => dispatch({ type: ACTIONS.GO_NEXT }),
-        prev: () => dispatch({ type: ACTIONS.GO_PREV }),
-        setAcross: isAcross => {
-          dispatch({ type: ACTIONS.SET_ACROSS, payload: { isAcross } });
-        },
-        setWord: (clue, word) => {
-          // TODO, This currently doesn't work for anything after first row.
-          // The lookup is slightly backwards.
-          const [clueNumStr, clueDirStr] = clue.match(/[a-z]+|[^a-z]+/gi);
-          const clueNum = parseInt(clueNumStr);
-          const isAcross =
-            clueDirStr === "A" || (state.isAcross && !clueDirStr);
+  useEffect(() => {
+    window.XW = {
+      next: () => dispatch({ type: ACTIONS.GO_NEXT }),
+      prev: () => dispatch({ type: ACTIONS.GO_PREV }),
+      setAcross: isAcross => {
+        dispatch({ type: ACTIONS.SET_ACROSS, payload: { isAcross } });
+      },
+      setWord: (clue, word) => {
+        // TODO, This currently doesn't work for anything after first row.
+        // The lookup is slightly backwards.
+        const [clueNumStr, clueDirStr] = clue.match(/[a-z]+|[^a-z]+/gi);
+        const clueNum = parseInt(clueNumStr);
+        const isAcross = clueDirStr === "A" || (state.isAcross && !clueDirStr);
+        const indexClue = clues[isAcross ? "across" : "down"].findIndex(str => {
+          return str.indexOf(`${clueNum}.`) === 0;
+        });
 
-          const index = cluesLookup[isAcross ? "across" : "down"].indexOf(
-            clueNum - 1
-          );
-
-          dispatch({ type: ACTIONS.SET_INDEX, payload: { index } });
-          dispatch({ type: ACTIONS.SET_ACROSS, payload: { isAcross } });
-
-          word.split("").forEach(letter => {
-            dispatch({
-              type: ACTIONS.SET_LETTERS,
-              payload: { letter }
-            });
-            dispatch({ type: ACTIONS.GO_NEXT });
-          });
-        }
-      };
-    },
-    [state.isAcross]
-  );
-
-  useEffect(
-    () => {
-      function handleKeyDown(event) {
-        if (event.target.className !== styles.input) {
-          return dispatch({ type: ACTIONS.SET_INDEX, payload: { index: -1 } });
+        const index = cluesLookup[isAcross ? "across" : "down"].indexOf(
+          indexClue
+        );
+        if (indexClue === -1) {
+          return console.warn("Incorrect clue");
         }
 
-        event.preventDefault();
+        dispatch({ type: ACTIONS.SET_INDEX, payload: { index } });
+        dispatch({ type: ACTIONS.SET_ACROSS, payload: { isAcross } });
 
-        if (utils.isLetter(event.key) || event.key === EVENTS.Backspace) {
+        word.split("").forEach(letter => {
           dispatch({
             type: ACTIONS.SET_LETTERS,
-            payload: { letter: event.key === EVENTS.Backspace ? "" : event.key }
+            payload: { letter }
           });
-        }
-
-        if (
-          event.key === EVENTS.ArrowLeft ||
-          event.key === EVENTS.ArrowRight ||
-          event.key === EVENTS.ArrowDown ||
-          event.key === EVENTS.ArrowUp
-        ) {
-          dispatch({
-            type: ACTIONS.SET_ACROSS,
-            payload: {
-              isAcross:
-                event.key === EVENTS.ArrowLeft ||
-                event.key === EVENTS.ArrowRight
-            }
-          });
-        }
-
-        if (
-          (state.isAcross && event.key === EVENTS.ArrowLeft) ||
-          (!state.isAcross && event.key === EVENTS.ArrowUp) ||
-          (event.key === EVENTS.Tab && event.shiftKey) ||
-          event.key === EVENTS.Backspace
-        ) {
-          dispatch({ type: ACTIONS.GO_PREV });
-        }
-
-        if (
-          (state.isAcross && event.key === EVENTS.ArrowRight) ||
-          (!state.isAcross && event.key === EVENTS.ArrowDown) ||
-          event.key === EVENTS.Tab ||
-          utils.isLetter(event.key)
-        ) {
           dispatch({ type: ACTIONS.GO_NEXT });
-        }
+        });
       }
-      window.addEventListener("keydown", handleKeyDown);
-      return () => window.removeEventListener("keydown", handleKeyDown);
-    },
-    [state.isAcross]
-  );
+    };
+  }, [state.isAcross]);
+
+  useEffect(() => {
+    function handleKeyDown(event) {
+      if (event.target.className !== styles.input) {
+        return dispatch({ type: ACTIONS.SET_INDEX, payload: { index: -1 } });
+      }
+
+      event.preventDefault();
+
+      if (utils.isLetter(event.key) || event.key === EVENTS.Backspace) {
+        dispatch({
+          type: ACTIONS.SET_LETTERS,
+          payload: { letter: event.key === EVENTS.Backspace ? "" : event.key }
+        });
+      }
+
+      if (
+        event.key === EVENTS.ArrowLeft ||
+        event.key === EVENTS.ArrowRight ||
+        event.key === EVENTS.ArrowDown ||
+        event.key === EVENTS.ArrowUp
+      ) {
+        dispatch({
+          type: ACTIONS.SET_ACROSS,
+          payload: {
+            isAcross:
+              event.key === EVENTS.ArrowLeft || event.key === EVENTS.ArrowRight
+          }
+        });
+      }
+
+      if (
+        (state.isAcross && event.key === EVENTS.ArrowLeft) ||
+        (!state.isAcross && event.key === EVENTS.ArrowUp) ||
+        (event.key === EVENTS.Tab && event.shiftKey) ||
+        event.key === EVENTS.Backspace
+      ) {
+        dispatch({ type: ACTIONS.GO_PREV });
+      }
+
+      if (
+        (state.isAcross && event.key === EVENTS.ArrowRight) ||
+        (!state.isAcross && event.key === EVENTS.ArrowDown) ||
+        event.key === EVENTS.Tab ||
+        utils.isLetter(event.key)
+      ) {
+        dispatch({ type: ACTIONS.GO_NEXT });
+      }
+    }
+    window.addEventListener("keydown", handleKeyDown);
+    return () => window.removeEventListener("keydown", handleKeyDown);
+  }, [state.isAcross]);
 
   return (
     <main className={styles.main}>
